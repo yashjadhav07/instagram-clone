7ad846ecdffbecc2c4e5b12bd7f61a5b843e382b
Yash Jadhav <53169635+yashjadhav07@users.noreply.github.com>
2021-07-19 14:17:50 +0530

    search bar icon added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,38 +4,73 @@ import {UserContext} from '../App'
 import { ExploreIcon } from "./Icons";
 import homelogo from "./imgs/home.png";
 import pluslogo from "./imgs/plus.png";
+import searchlogo from "./imgs/search.png";
 import { Navbar, Nav, Container, Button, Dropdown, DropdownButton, Row, Col } from 'react-bootstrap';
 
+const Search=()=>{
+  const [query,setQuery] = useState("")
+  const [ans,setAns] = useState([])
+  const {state} = useContext(UserContext)
 
+  useEffect(()=>{
+      var temp_q = query
+      if(temp_q==="") temp_q="-"
+      fetch('/search-users',{
+          method:"post",
+          headers:{
+              "Content-Type":"application/json",
+              "Authorization":"Bearer "+localStorage.getItem("jwt")
+          },
+          body:JSON.stringify({
+              query:temp_q
+          })
+      }).then(res=>res.json())
+      .then(result=>{
+        setAns(result.user)
+        console.log(result)
+      }).catch(err=>{
+          console.log(err)
+      })
+    },[query])
+  return(
+    <Container style={{marginTop: "70px", marginLeft: "560px", width: "30%"}} className="home">
+      <Row>
+        <Col style={{paddingRight: "0px"}} sm={8}>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search here.."
+            value={query}
+            onChange={(e)=>setQuery(e.target.value)}
+          />
+        </Col>
+        <Col style={{paddingLeft: "5px"}} sm={4}>
+          <DropdownButton id="dropdown-basic-button" title="Search">
+          {
+            ans.map(item=>{
+             return(
+               <Dropdown.Item><Link to={item._id !== state._id?"/profile/"+item._id :"/profile" }>
+                 {item.email}</Link></Dropdown.Item>
+             )
+           }
+           )
+          }
+          </DropdownButton>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
 
 const NavBar = ()=>{
-     const [query,setQuery] = useState("")
-     const [ans,setAns] = useState([])
 
+     const [showSearch, setShowSearch] = useState(false)
      const {state,dispatch} = useContext(UserContext)
-     useEffect(()=>{
-         var temp_q = query
-         if(temp_q==="") temp_q="-"
-         fetch('/search-users',{
-             method:"post",
-             headers:{
-                 "Content-Type":"application/json",
-                 "Authorization":"Bearer "+localStorage.getItem("jwt")
-             },
-             body:JSON.stringify({
-                 query:temp_q
-             })
-         }).then(res=>res.json())
-         .then(result=>{
-           setAns(result.user)
-           console.log(result)
-         }).catch(err=>{
-             console.log(err)
-         })
-       },[query])
+
      const history = useHistory()
       return(
-          <Navbar collapseOnSelect fixed='top' expands='sm' bg='light' variant='dark'>
+        <div>
+          <Navbar collapseOnSelect fixed='top' expands='sm' style={{backgroundColor: "rgb(114,163,155)"}} variant='light'>
             <Container>
               <Navbar.Toggle aria-controls='responsive-navbar-nav' />
               <Navbar.Collapse id='responsive-navbar-nav'>
@@ -89,33 +124,20 @@ const NavBar = ()=>{
                     </Nav.Link>
                   </Nav.Item>
                   <Nav.Item className="ml-auto">
-                    <Row>
-                      <Col style={{paddingRight: "0px"}} sm={8}>
-                        <input
-                          type="text"
-                          className="form-control"
-                          placeholder="Search here.."
-                          value={query}
-                          onChange={(e)=>setQuery(e.target.value)}
-                        />
-                      </Col>
-                      <Col style={{paddingLeft: "5px"}} sm={4}>
-                        <DropdownButton id="dropdown-basic-button" title="Search">
-                        {
-                          ans.map(item=>{
-                           return(
-                             <Dropdown.Item><Link to={item._id !== state._id?"/profile/"+item._id :"/profile" }>
-                               {item.email}</Link></Dropdown.Item>
-                           )
-                         }
-                         )
-                        }
-                        </DropdownButton>
-                      </Col>
-                    </Row>
-
+                      <img
+                        style={{
+                          width: "24px",
+                          height: "24px",
+                          objectFit: "cover",
+                          borderRadius: "12px",
+                        }}
+                        src={searchlogo}
+                        alt="searchlogo"
+                        onClick={()=>setShowSearch(!showSearch)}
+                      />
 
                   </Nav.Item>
+
                   <Nav.Item className="ml-auto">
                     <Button variant='outline-danger' onClick={()=>{
                              localStorage.clear()
@@ -142,6 +164,10 @@ const NavBar = ()=>{
             </Container>
           </Navbar>
 
+          {
+            showSearch ? <Search/> : null
+          }
+        </div>
     )
 
 }
